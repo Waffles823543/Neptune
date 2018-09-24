cc579c25a07bd2fb4dcab03340197232829f735b
Raf <rafael.deubler@gmail.com>
2018-09-25 08:29:14 +1000

    Initial Commit

---
+++ public/index.js
@@ -0,0 +1,23 @@
+function send() {
+    var toSend = $("#message").val();
+    var author = $("#username").val();
+    $.get("./api/send/" + author + "/" + toSend, data => {})
+
+    $("#message").val("");
+}
+
+console.log("hello");
+
+window.setInterval(() => {
+    $.get("./api/messages", data => {
+        var formattedData = [];
+        for (var i = 0; i < data.length; i++) {
+            if (data[i] != "\n") {
+                formattedData.push(data[i])
+            } else {
+                formattedData.push("</br>")
+            }
+        }
+        $("p").html(formattedData.join(""));
+    })
+}, 100)
